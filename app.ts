2d26e511b569b7a0d62a5edc5f719b54df807fd3
Vasiliy Prokofiev <Takayuki.sempai@gmail.com>
2024-10-21 05:41:14 +0300

    feat: implement post mapping end parsing body

--- app.ts
+++ app.ts
@@ -4,11 +4,15 @@ import {ensureNumber} from "./src/util/utils.js";
 const server = Server()
 const port = ensureNumber(process.env.PORT) || 3000;
 
-server.get("/test/{par}", (req, res, params) => {
+server.get("/test/{par}", async (req, res, params) => {
     console.log(req, res, params)}
 )
-server.get("/test/{par}/{par2}", (req, res, params) => {
+server.get("/test/{par}/{par2}", async (req, res, params) => {
     console.log(req, res, params)}
+)
+
+server.post("/test", async (req, res, params, body: string) => {
+    console.log(req, res, params, body)}
 )
 
 server.listen(port, () => {
--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,8 @@ import * as http from "node:http";
 import {IncomingMessage, ServerResponse} from "http";
 import {ensureExists} from "./util/utils.js";
 
-type UrlHandler = (request: IncomingMessage, response: ServerResponse, params: Map<string, string>) => void
+type UrlHandler = (request: IncomingMessage, response: ServerResponse, params: Map<string, string>) => Promise<void>
+type PostHandler = (request: IncomingMessage, response: ServerResponse, params: Map<string, string>, body: string) => Promise<void>
 
 type Route = {
     testUrl: RegExp,
@@ -13,15 +14,16 @@ type Route = {
 const Server = () => {
     const methodRoutes = {
         "GET": [] as Route[],
+        "POST": [] as Route[],
     };
-    const server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
+    const server = http.createServer(async (request: IncomingMessage, response: ServerResponse) => {
         try {
-            const method = ensureExists(req.method, () => `Can't handle request without method url: ${req.url}`)
+            const method = ensureExists(request.method, () => `Can't handle request without method url: ${request.url}`)
             const routes = ensureExists(
                 methodRoutes[method.toUpperCase() as keyof typeof methodRoutes],
-                () => `Can't handle request with method ${req.method}, url: ${req.url}`
+                () => `Can't handle request with method ${request.method}, url: ${request.url}`
             )
-            const url = ensureExists(req.url, () => `url not found in request ${req}`)
+            const url = ensureExists(request.url, () => `url not found in request ${request}`)
             const route = ensureExists(
                 routes.find(route => url.match(route.testUrl)),
                 () => `Handler not found for url ${url}`
@@ -33,29 +35,51 @@ const Server = () => {
                     resultParams.set(paramName, execResult[index + 1])
                 })
             }
-            route.handler(req, res, resultParams)
-            res.end()
+            route.handler(request, response, resultParams)
+            response.end()
         } catch (e: any) {
             const message = e instanceof Error ? e.message : 'Unknown Error'
             message && console.log(message)
-            res.statusCode = 404;
-            res.end()
+            response.statusCode = 404;
+            response.end()
         }
     })
 
-    const get = (url: string, handler: UrlHandler) => {
+    const getBody = (request: IncomingMessage): Promise<string> => {
+        return new Promise((resolve) => {
+            const bodyParts: Uint8Array[] = [];
+            let body;
+            request.on('data', (chunk) => {
+                bodyParts.push(chunk);
+            }).on('end', () => {
+                body = Buffer.concat(bodyParts).toString();
+                resolve(body)
+            });
+        });
+    }
+
+    const prepareRoute = (url: string, handler: UrlHandler): Route => {
         const regexp = new RegExp("({[^{}]*})", "g")
         const urlParams = url.match(regexp)
         const params = urlParams?.map(param => param.slice(1, -1)) || []
         const testUrl = new RegExp("^" + url.replaceAll("/", "\\/").replaceAll(regexp, "([^\\/]*)") + "$", "g")
-        methodRoutes["GET"].push({testUrl, params, handler})
+        return {testUrl, params, handler}
+    }
+
+    const get = (url: string, handler: UrlHandler) => {
+        methodRoutes["GET"].push(prepareRoute(url, handler))
+    }
+    const post = (url: string, handler: PostHandler) => {
+        const handlerWithBody = async (request: IncomingMessage, response: ServerResponse, params: Map<string, string>) =>
+            handler(request, response, params, await getBody(request))
+        methodRoutes["POST"].push(prepareRoute(url, handlerWithBody))
     }
 
     const listen = (port: number, handler: () => void) => {
         server.listen(port, handler)
     }
 
-    return {get, listen}
+    return {get, post, listen}
 }
 
 export {Server}
