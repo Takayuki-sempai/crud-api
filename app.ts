d8de522e41b0a9b15121b4e896eae109cda82094
Vasiliy Prokofiev <Takayuki.sempai@gmail.com>
2024-10-22 00:10:24 +0300

    refactor: combine handlers to controller

--- app.ts
+++ app.ts
@@ -1,13 +1,14 @@
 import {Server} from "./src/server/server.ts";
 import {ensureNumber} from "./src/util/utils.js";
-import {addUser, getAllUsers, getUserById} from "./src/controller/users.js";
+import {UsersController} from "./src/controller/users.js";
 
 const server = Server()
+const controller = UsersController()
 const port = ensureNumber(process.env.PORT) || 3000;
 
-server.get("/api/users", getAllUsers)
-server.get("/api/users/{userId}", getUserById)
-server.post("/api/users", addUser)
+server.get("/api/users", controller.getAllUsers)
+server.get("/api/users/{userId}", controller.getUserById)
+server.post("/api/users", controller.addUser)
 
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
--- src/controller/users.ts
+++ src/controller/users.ts
@@ -7,27 +7,35 @@ import {validate} from "uuid";
 
 const users: User[] = []
 
-export const getAllUsers: UrlHandler = async (_: Map<string, string>): Promise<HandlerResponse> => {
-   return createOkResponse(users)
-}
+export const UsersController = () => {
+    const getAllUsers: UrlHandler = async (_: Map<string, string>): Promise<HandlerResponse> => {
+        return createOkResponse(users)
+    }
 
-export const getUserById: UrlHandler = async (params: Map<string, string>): Promise<HandlerResponse> => {
-    const userId = params.get("userId")
-    if(!userId) {
-        throw new InternalError("Param userId not found")
+    const getUserById: UrlHandler = async (params: Map<string, string>): Promise<HandlerResponse> => {
+        const userId = params.get("userId")
+        if (!userId) {
+            throw new InternalError("Param userId not found")
+        }
+        if (!validate(userId)) {
+            throw new RequestParseError("UserId id not uuid type")
+        }
+        const foundUser = users.find(user => user.id === userId)
+        if (!foundUser) {
+            throw new NotFoundError(`User with id: ${userId} not found`)
+        }
+        return createOkResponse(foundUser)
     }
-    if(!validate(userId)) {
-        throw new RequestParseError("UserId id not uuid type")
-    }
-    const foundUser = users.find(user => user.id === userId)
-    if(!foundUser) {
-        throw new NotFoundError(`User with id: ${userId} not found`)
-    }
-    return createOkResponse(foundUser)
-}
 
-export const addUser: PostHandler = async (body: object): Promise<HandlerResponse> => {
-    const newUser = toUserMode(body)
-    users.push(newUser)
-    return createResponse(newUser, HttpResponseCode.CREATED)
+    const addUser: PostHandler = async (body: object): Promise<HandlerResponse> => {
+        const newUser = toUserMode(body)
+        users.push(newUser)
+        return createResponse(newUser, HttpResponseCode.CREATED)
+    }
+
+    return {
+        getAllUsers,
+        getUserById,
+        addUser,
+    }
 }
